55476c08bf1e1f04f1dad607d23e2954d70b9828
sailnov <mkouki1959@gmail.com>
2025-05-22 01:03:04 +0900

    first commit

---
+++ WEBSite20250520/shimonishi/DL8/data_ao.js
@@ -0,0 +1,12 @@
+// DL8 Type-D
+// Ver 3.3.94
+var ao_chs = 0;
+var ao_ch = ["",""];
+var ao_name = ["",""];
+var ao_comm = ["",""];
+var ao_unit = ["",""];
+var ao_real = ["",""];
+var ao_chno = [0,0];
+var ao_enable = [0,0];
+var ao_lower = ["",""];
+var ao_upper = ["",""];
